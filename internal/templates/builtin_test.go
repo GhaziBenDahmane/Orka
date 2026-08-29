01e8c3c9390c83787a079a46a8a9ad33c1a3a34c
Ghazi BEN DAHMANE <ghazi.bendahmane@amundi.com>
2026-08-29 21:03:45 +0200

    chore: update BarkTrace templates to v0.15.0

--- internal/templates/builtin_test.go
+++ internal/templates/builtin_test.go
@@ -24,8 +24,8 @@ func TestBarkTraceTemplatePinsReleasedImage(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		if !strings.Contains(string(definition), `barktrace_version = "0.13.0"`) {
-			t.Fatalf("%s template must pin the released 0.13.0 image", name)
+		if !strings.Contains(string(definition), `barktrace_version = "0.15.0"`) {
+			t.Fatalf("%s template must pin the released 0.15.0 image", name)
 		}
 	}
 }
