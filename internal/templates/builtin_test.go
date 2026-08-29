165a6bf833fa493855cc226d067cc02b4edefabb
Ghazi BEN DAHMANE <ghazi.bendahmane@amundi.com>
2026-08-29 17:39:15 +0200

    pin BarkTrace template release image

--- internal/templates/builtin_test.go
+++ internal/templates/builtin_test.go
@@ -1,6 +1,8 @@
 package templates
 
 import (
+	"io/fs"
+	"strings"
 	"testing"
 
 	"github.com/bendahma/dokploy-go/internal/deploy"
@@ -13,5 +15,15 @@ func TestBuiltinCatalogIsSwarmSafe(t *testing.T) {
 	}
 	if report.Imported < 4 || len(report.Failed) > 0 {
 		t.Fatalf("report=%#v", report)
+	}
+}
+
+func TestBarkTraceTemplatePinsReleasedImage(t *testing.T) {
+	definition, err := fs.ReadFile(builtinCatalog, "builtin/blueprints/barktrace-sqlite/template.toml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !strings.Contains(string(definition), `barktrace_version = "sha-071bf79"`) {
+		t.Fatal("BarkTrace template must pin the released sha-071bf79 image")
 	}
 }
